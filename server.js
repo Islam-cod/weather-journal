af54aaa340bb5c6c42c7619282a40997a3dc8d37
Islam-cod <74316365+Islam-cod@users.noreply.github.com>
2022-03-13 00:55:50 +0100

    second commit

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require('body-parser')
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +16,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors")
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+function listening(){
+    console.log("server running"); 
+    console.log(`running on localhost: {$port}`);
+}
+
+// Create a get route that respnds with all required data
+app.get("/results", sendData);
+
+const sendData = (req, res) => res.status(200).send(projectData);
+
+// Create a post route that adds the data to the Server
+app.post("/add", addData);
+
+const addData = (req, res) => {
+    projectData = req.body;
+    console.log(projectData);
+    res.status(200).send(projectData);
+  };
